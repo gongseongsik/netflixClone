c2157f1257b01eae634ef4614ab03e73d8ac310a
kongseongsik <rhdtjdtlr333@naver.com>
2022-09-04 19:01:55 +0900

    NetflixClone 커밋

---
+++ app/src/main/java/com/netflixclone/MyApplication.kt
@@ -0,0 +1,17 @@
+package com.netflixclone
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+
+
+open class MyApplication : Application() {
+    override fun onCreate() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        super.onCreate()
+
+//        try {
+//            YoutubeDL.getInstance().init(this)
+//        } catch (e: YoutubeDLException) {
+//        }
+    }
+}
